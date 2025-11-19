6dd597a6c774c2927a33bc58ad203984cea9236e
sooraj aryan <soorajaryan007@gmail.com>
2025-11-19 17:43:56 +0530

    Fix Google Ads crossorigin

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
      <script
   async
   src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7999471710145982"
-  crossOrigin="anonymous"
+ crossOrigin="anonymous"
 ></script>
 
 
